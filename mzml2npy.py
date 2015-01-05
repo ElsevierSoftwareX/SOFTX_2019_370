0e5a63088daf5631d8cc2dad07505636cdb086a5
Luke Zappia <luke@lazappi.id.au>
2015-01-06 10:03:28 +1100

    Add comments to 'mzml2npy.py'.

--- mzml2npy.py
+++ mzml2npy.py
@@ -7,6 +7,8 @@ import argparse
 import pymzml
 import numpy as np
 
+# Parse command line arguaments
+
 parser = argparse.ArgumentParser(description='Convert an mzML file to NumPy arrays.')
 
 parser.add_argument('inputFile',
@@ -21,6 +23,13 @@ parser.add_argument('-o',
 
 opts = parser.parse_args()
 
+# Find the number of spectra in the file and the number of points in the
+# biggest spectra in order to determine the size of the NumPy matrices
+
+# Exclude the last spectrum as it has large values inconsistent with the
+# rest of the data
+# This may just be a problem with the test data we are using
+
 ms_run = pymzml.run.Reader(opts.inputFile)
 mz_lengths = []
 spectrum_count = 0
@@ -29,18 +38,20 @@ for spectrum in ms_run:
     spectrum_count += 1
     mz_lengths.append(len(spectrum.mz))
 
-# Exclude the last spectrum as it has large values inconsistent with the
-# rest of the data
-# This may just be a problem with the test data we are using
 max_mz_length = max(mz_lengths[:-1])
 
-ms_run = pymzml.run.Reader(opts.inputFile)
+# Create empty matrices using the determined dimensions. Missing values
+# will remain as NaN
 
 mzs = np.full((max_mz_length, spectrum_count-1), np.nan, dtype='float64')
 intensities = np.full((max_mz_length, spectrum_count-1), np.nan,
                       dtype='float64')
 times = np.full((max_mz_length, spectrum_count-1), np.nan, dtype='float64')
 
+# Iterate over the spectra in the file and insert values into appropriate
+# locations in the matrices
+
+ms_run = pymzml.run.Reader(opts.inputFile)
 spectrum_idx = 0
 
 while spectrum_idx < spectrum_count-1:
@@ -50,6 +61,8 @@ while spectrum_idx < spectrum_count-1:
     times[0:len(spectrum.i), spectrum_idx] = np.array(
         spectrum['MS:1000016'] * len(spectrum.i))
     spectrum_idx += 1
+
+# Output the NumPy arrays
 
 file_prefix = opts.inputFile.split('/')[-1].split('.')[0]
 
