28c4709d5984feb2dc955aa629016da4c497f8cc
Luke Zappia <lzappia@student.unimelb.edu.au>
2015-01-22 13:46:05 +1100

    Archive test code

--- hitime.cpp
+++ hitime.cpp
@@ -76,40 +76,16 @@ int main(int argc, char *argv[])
    
     pwiz::msdata::FullReaderList readers;
     pwiz::msdata::MSDataFile msd(opts.mzML_file, &readers);
-
-    
-    std::cout << "Timestamp: " << msd.run.startTimeStamp << std::endl;
-
     pwiz::msdata::SpectrumList& spectrumList = *msd.run.spectrumListPtr;
     const bool getBinaryData = true;
-    size_t numSpectra = spectrumList.size();
-
-    std::cout << "Num Spectra: " << numSpectra << std::endl;
     
     pwiz::msdata::SpectrumPtr spectrum;
     std::vector<pwiz::msdata::MZIntensityPair> pairs;
-    spectrum = spectrumList.spectrum(0, getBinaryData);
-    spectrum->getMZIntensityPairs(pairs);
-
-    std::cout << "Num Pairs: " << pairs.size() << std::endl;
-
     pwiz::msdata::MZIntensityPair pair;
-    pair = pairs[0];
-
-    std::cout << "MZ/Int Pair: " << pair.mz << " " << pair.intensity; 
-    std::cout << std::endl;
 
     pwiz::analysis::SpectrumList_MZWindow mz_window(msd.run.spectrumListPtr,
                                                     150.2, 150.3);
-    spectrum = mz_window.spectrum(0, getBinaryData);
-    spectrum->getMZIntensityPairs(pairs);
-    pair = pairs[0];
-
-    std::cout << "WINDOW 1 (150.2 - 150.3): " << std::endl;
-    std::cout << "Num Pairs: " << pairs.size() << std::endl;
-    std::cout << "MZ/Int Pair: " << pair.mz << " " << pair.intensity; 
-    std::cout << std::endl;
-
+    
     std::cout << "Done!" << std::endl;
 
     return 0;
@@ -235,158 +211,44 @@ Options::Options(int argc, char *argv[])
 /*-----------------------------------------------------------------------*/
 
 /*
-class MZWindow {
+    Options opts(argc, argv);
+   
+    pwiz::msdata::FullReaderList readers;
+    pwiz::msdata::MSDataFile msd(opts.mzML_file, &readers);
+
     
-    public:
-        unsigned int centre_col;
-        double centre_mz;
-        float tolerance;
-        unsigned int col_half_length;
-        std::vector<int> row_lower_bound;
-        std::vector<int> row_upper_bound;
+    std::cout << "Timestamp: " << msd.run.startTimeStamp << std::endl;
 
-    void SetBounds (arma::mat matrix, unsigned int col,
-                    unsigned int half, double mz, float tol) 
-    {
-        centre_col      = col;
-        col_half_length = half;
-        centre_mz       = mz;
-        tolerance       = tol;
-        
-        unsigned int start_col = std::max(0U, centre_col - col_half_length);
-        unsigned int end_col = std::min(centre_col + col_half_length, 
-                                        matrix.n_cols-1);
-        arma::uvec indexes;
+    pwiz::msdata::SpectrumList& spectrumList = *msd.run.spectrumListPtr;
+    const bool getBinaryData = true;
+    size_t numSpectra = spectrumList.size();
 
-        for (int col_idx = start_col; col_idx <= end_col; col_idx++) {
-            
-            indexes = arma::find(matrix.col(col_idx) > centre_mz - tolerance
-                            && matrix.col(col_idx) < centre_mz + tolerance);
-            
-            if (indexes.n_elem > 0) {
-                row_lower_bound.push_back(indexes(0));
-                row_upper_bound.push_back(indexes(indexes.n_elem-1));
-            } else {
-                row_lower_bound.push_back(-1);
-                row_upper_bound.push_back(-1);
-            }
-        }
-    }
-
-    void PrintBounds ()
-    {
-        for (std::vector<int>::const_iterator i = row_lower_bound.begin();
-             i != row_lower_bound.end(); ++i) {
-            std::cout << (*i) << " ";
-        }
-
-        std::cout << std::endl;
-        for (std::vector<int>::const_iterator i = row_upper_bound.begin(); 
-             i != row_upper_bound.end(); ++i) {
-            std::cout << (*i) << " ";
-        }
-        std::cout << std::endl;
-    }
-
-};
-
-class DoubleWindow {
-
-    public:
-        MZWindow lo_window;
-        MZWindow hi_window;
-
-    void SetWindows (arma::mat matrix, unsigned int col, unsigned int half,
-                     double mz, float delta, float lo_tol, float hi_tol)
-    {
-        lo_window.SetBounds(matrix, col, half, mz, lo_tol);
-        hi_window.SetBounds(matrix, col, half, mz+delta, hi_tol);
-    }
-
-};
-
-*/
-
-/**
-  Read in a NumPy array and return an Armadillo matrix
-*/
-/*
-arma::mat load_npy_file(const std::string& filename)
-{
-    std::vector<int> shape;
-    std::vector<double> data;
+    std::cout << "Num Spectra: " << numSpectra << std::endl;
     
-    aoba::LoadArrayFromNumpy(filename, shape, data);
+    pwiz::msdata::SpectrumPtr spectrum;
+    std::vector<pwiz::msdata::MZIntensityPair> pairs;
+    spectrum = spectrumList.spectrum(0, getBinaryData);
+    spectrum->getMZIntensityPairs(pairs);
 
-    int nrows = shape[0];
-    int ncols = shape[1];
-    int row   = 0;
-    int col   = 0;
-    std::vector<double>::const_iterator i;
+    std::cout << "Num Pairs: " << pairs.size() << std::endl;
 
-    arma::mat matrix(nrows, ncols);
+    pwiz::msdata::MZIntensityPair pair;
+    pair = pairs[0];
 
-    for (i = data.begin(); i != data.end(); i++) {
-        matrix(row, col) = (*i);
-        col++;
-        if (col == ncols) {
-            col = 0;
-            row++;
-        }
-    }
+    std::cout << "MZ/Int Pair: " << pair.mz << " " << pair.intensity; 
+    std::cout << std::endl;
 
-    return matrix;
-}
+    pwiz::analysis::SpectrumList_MZWindow mz_window(msd.run.spectrumListPtr,
+                                                    150.2, 150.3);
+    spectrum = mz_window.spectrum(0, getBinaryData);
+    spectrum->getMZIntensityPairs(pairs);
+    pair = pairs[0];
+
+    std::cout << "WINDOW 1 (150.2 - 150.3): " << std::endl;
+    std::cout << "Num Pairs: " << pairs.size() << std::endl;
+    std::cout << "MZ/Int Pair: " << pair.mz << " " << pair.intensity; 
+    std::cout << std::endl;
+
+    std::cout << "Done!" << std::endl;
 */
-/*
-    std::cout << "File Directory: " << file_dir << std::endl;
-    std::cout << "MZ File: " << mz_file << std::endl;
-    std::cout << "Time File: " << time_file << std::endl;
-    std::cout << "Intensity File: " << intensity_file << std::endl;
- 
-*/
-/*
-    arma::mat mz_mat = load_npy_file(mz_file);
-//    mz_mat.submat(0, 0, 5, 5).print("MZ Data:");
 
-    arma::mat time_mat = load_npy_file(time_file);
-//    time_mat.submat(0, 0, 5, 5).print("Time Data:");
-
-    arma::mat intensity_mat = load_npy_file(intensity_file);
-//    intensity_mat.submat(0, 0, 5, 5).print("Intensity Data:");
-
-
-//    half_rt_window = static_cast<int>(ceil(rt_sigma * rt_width / 2.355));
-
-
-    arma::mat C;
-    C << 0.11 << 0.01 << 0.09 << 0.10 << 0.12 << 0.08 << 0.09 << arma::endr
-      << 0.19 << 0.14 << 0.23 << 0.19 << 0.28 << 0.16 << 0.24 << arma::endr
-      << 0.31 << 0.26 << 0.27 << 0.34 << 0.32 << 0.28 << 0.32 << arma::endr
-      << 0.43 << 0.38 << 0.41 << 0.48 << 0.38 << 0.43 << 0.41 << arma::endr
-      << 0.49 << 0.54 << 0.46 << 0.53 << 0.51 << 0.50 << 0.49 << arma::endr
-      << 0.62 << 0.64 << 0.58 << 0.61 << 0.66 << 0.58 << 0.63 << arma::endr
-      << 0.70 << 0.68 << 0.71 << 0.70 << 0.72 << 0.69 << 0.66 << arma::endr
-      << 0.76 << 0.81 << 0.79 << 0.77 << 0.83 << 0.84 << 0.81 << arma::endr
-      << 0.89 << 0.92 << 0.96 << 0.89 << 0.91 << 0.95 << 0.92 << arma::endr;
-
-    C.print("C: ");
-
-    MZWindow window;
-    //window.SetBounds(C, 2, 3, 0.15, 2);
-
-    //arma::mat D = mz_mat.submat(20, 20, 30, 24);
-    //D.print("D:");
-
-    //MZWindow wind2;
-    //wind2.SetBounds(D, 4, 3, D(3,4), 0.02);
-    //wind2.PrintBounds();
-
-    //time_mat.submat(20, 20, 30, 24).print("Time: ");
-
-    DoubleWindow dwind;
-    dwind.SetWindows(C, 3, 2, C(2, 3), 0.3, 0.11, 0.16); 
-    dwind.lo_window.PrintBounds();
-    dwind.hi_window.PrintBounds();
-
-*/
