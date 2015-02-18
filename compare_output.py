a87d9405421c55696cc7931fc53e0c2df1eb5b9f
Luke Zappia <luke@lazappi.id.au>
2015-02-18 16:32:26 +1100

    Add Doxygen comments

--- compare_output.py
+++ compare_output.py
@@ -1,5 +1,13 @@
 #!/bin/env python
 
+## @package compare_output
+# Program for comparing HiTIME output.
+#
+# Primarily designed for comparing output from the Python and C++ verions of
+# HiTIME. The program identifies points that have a zero score in one output
+# and not the other, calculates errors between the two outputs and produces
+# a series of useful plots.
+
 import sys
 import argparse
 import numpy as np
@@ -9,16 +17,17 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def main():
 
+    # Read command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('output1', help='path to first output file')
     parser.add_argument('output2', help='path to second output file')
     args = parser.parse_args()
 
     print "Loading data..."
-
     data1 = read_data(args.output1)
     data2 = read_data(args.output2)
 
+    # Check output files have the same number of points, if not exit
     if not len(data1) == len(data2):
         print "Output files have different lengths and cannot be compared"
         print "Try producing full output"
@@ -26,24 +35,24 @@ def main():
         sys.exit()
 
     print "Comparing zero score points..."
-
     compare_zeros(data1, data2)
 
+    # Convert to numpy arrays for remaining processing
     data1 = np.array(data1)
     data2 = np.array(data2)
 
     print "Computing statistics..."
-    
     calc_stats(data1, data2)
 
+    # Brewer colours used for plotting
     dark2_3 = [(27, 158, 119), (217, 95, 2), (117, 112, 179)]
 
+    # Convert from 0-255 to 0-1, required by matplotlib
     for i in range(len(dark2_3)):
         r, g, b = dark2_3[i]
         dark2_3[i] = (r / 255., g / 255., b / 255.)
 
     print "Creating plots..."
-
     plot_line_comp(data1, data2, dark2_3)
     plot_peaks(data1, data2)
     plot_score_comp(data2, data2, dark2_3)
@@ -51,7 +60,11 @@ def main():
 
     print "Done!"
 
-
+## Read data from a HiTIME output file.
+#
+# @param datafile Path to a HiTIME output file.
+#
+# @return List of lists where each contains a line of data
 def read_data(datafile):
     
     data_list = []
@@ -64,9 +77,12 @@ def read_data(datafile):
 
     return data_list
 
-
+## Indentify points that are zero in one data set and not in the other.
+#
+# @param data1 First list of lists containing HiTIME data
+# @param data2 Second list of lists containing HiTIME data 
 def compare_zeros(data1, data2):
-    
+   
     zero_count = 0
 
     for rowi in xrange(len(data1)):
@@ -91,18 +107,24 @@ def compare_zeros(data1, data2):
     print "These are points that have a zero score in one file and not the" \
           "other"
 
-
+## Calculate difference and RMSE stats comparing two dataset. Results are 
+# printed.
+#
+# @param data1 First numpy array containing HiTIME data
+# @param data2 Second numpy array containing HiTIME data 
 def calc_stats(data1, data2):
 
+    # Find difference between values
     diff = data1 - data2
 
+    # Find average differences
     avg_diff = np.mean(diff, axis=0)
     cols = ['RT', 'MZ', 'Amp', 'Min Score', 'AB', 'A0', 'B0', 'r1']
 
     print "     AVERAGE DIFFERENCES    "
     print "----------------------------"
     for avg, col in zip(avg_diff, cols):
-        print "{0:<10}: {1:>12.5f}".format(col, avg)
+        print "{0:<10}: {1:>16.10f}".format(col, avg)
 
     print
     print "            RMSE            "
@@ -111,8 +133,16 @@ def calc_stats(data1, data2):
         rmse = np.sqrt(np.mean((data2[:, coli] - data1[:, coli]) ** 2))
         print "{0:<10}: {1:>16.10f}".format(cols[coli], rmse)
 
+
+## Plot line chart showing difference between datasets. Data is compressed
+# in the RT dimension. Result saved as 'line_comparision.png'.
+#
+# @param data1 First numpy array containing HiTIME data
+# @param data2 Second numpy array containing HiTIME data
+# @param colours List of (R, G, B) colours used for plotting 
 def plot_line_comp(data1, data2, colours):
 
+    # Extract data
     mz1 = data1[:, 1]
     ms1 = data1[:, 3]
     mz2 = data2[:, 1]
@@ -142,9 +172,17 @@ def plot_line_comp(data1, data2, colours):
 
     plt.plot(mz1, ms1, lw=2.5, color=colours[0])
     plt.plot(mz2, ms2, lw=2.5, color=colours[2])
+    
     plt.savefig('line_comparison.png', bbox_inches='tight')
+    
     plt.close()
 
+
+## Plot intensity peaks to demonstrate the original data. Results saved as
+# 'intensity_peaks.png'.
+#
+# @param data1 First numpy array containing HiTIME data
+# @param data2 Second numpy array containing HiTIME data 
 def plot_peaks(data1, data2):
 
     fig_peaks = plt.figure()
@@ -171,6 +209,13 @@ def plot_peaks(data1, data2):
     
     plt.close()
 
+
+## Plot 3D scatter plot to compare datasets. Result saved as 
+# 'score_comparision.png'.
+#
+# @param data1 First numpy array containing HiTIME data
+# @param data2 Second numpy array containing HiTIME data
+# @param colours List of (R, G, B) colours used for plotting 
 def plot_score_comp(data1, data2, colours):
 
     fig_comp = plt.figure()
@@ -212,6 +257,13 @@ def plot_score_comp(data1, data2, colours):
 
     plt.close()
 
+
+## Plot scatter plot showing difference between datasets. Data is compressed
+# in the RT dimension and each dot show the difference between the datasets
+# at that point. Result saved as 'score_difference.png'.
+#
+# @param data1 First numpy array containing HiTIME data
+# @param data2 Second numpy array containing HiTIME data 
 def plot_score_diff(data1, data2):
 
     fig_diff = plt.figure()
