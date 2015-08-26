ee8f3a6016a690b6542128341ef99a5e2ec9f3dc
Bernie Pope <bjpope@unimelb.edu.au>
2015-08-26 13:56:07 +1000

    Factor scoring code to its own module

---
+++ src/score.h
@@ -0,0 +1,30 @@
+/*
+#include <iostream>
+#include <unistd.h>
+#include <math.h>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <stdexcept>
+#include <functional>
+#include <fstream>
+#include <numeric>
+#include <iostream>
+#include <OpenMS/KERNEL/OnDiscMSExperiment.h>
+#include <OpenMS/FORMAT/IndexedMzMLFileLoader.h>
+#include "vector.h"
+
+using namespace OpenMS;
+using namespace std;
+*/
+
+
+//! @brief Calculate correlation scores for a window of data.
+
+/*
+double_2d score_spectra(pwiz::msdata::MSDataFile &msd, int centre_idx,
+                        int half_window, Options opts);
+*/
+
