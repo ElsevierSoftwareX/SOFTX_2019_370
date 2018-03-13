2504603451a6c48703799e31f9641855d8069b43
Bernie Pope <bjpope@unimelb.edu.au>
2018-03-13 21:45:34 +1100

    Process remaining arguments in docker wrapper

--- hitime-docker.sh
+++ hitime-docker.sh
@@ -4,6 +4,9 @@ set -e
 program_name="hitime-docker.sh"
 input_filepath=""
 output_filepath=""
+# this should be one of "score" or "max"
+mode=""
+remaining_args=""
 
 # Help message for using the program.
 function show_help {
@@ -12,7 +15,7 @@ cat << UsageMessage
 ${program_name}: detect twin ion signals in Mass Spectrometry data 
 
 Usage:
-    ${program_name} [-h] -i input.mzML -o output.mzML 
+    ${program_name} [-h] (score|max) -i input.mzML -o output.mzML 
 
 XXX description here
 
@@ -34,11 +37,29 @@ absolute_filepath () {
 
 # Parse the command line arguments and set the global variables language and new_project_name
 function parse_args {
+
+    if [ "$#" -lt 1 ]; then
+        show_help
+        exit 2 
+    fi
+
+    if [ "$1" = 'score' ]; then
+        shift 1
+        mode='score'
+    elif [ "$1" = 'max' ]; then
+        shift 1
+        mode='max'
+    fi 
+
     local OPTIND opt
 
     while getopts "hi:o:" opt; do
         case "${opt}" in
             h)
+                # Print mode specific help if mode is defined
+                if [[ -n ${mode} ]]; then
+                    exec docker run --rm bjpop/hitime ${mode} -h
+                fi
                 show_help
                 exit 0
                 ;;
@@ -53,13 +74,20 @@ function parse_args {
 
     [ "$1" = "--" ] && shift
 
+    remaining_args=$@
+
+    if [[ -z ${mode} ]]; then
+        exit_with_error "mode not specified, must be one of 'score' or 'max', use -h for help" 2
+    fi
+
     if [[ -z ${input_filepath} ]]; then
-        exit_with_error "missing command line argument: -i input.mzML, use -h for help" 2
+        exit_with_error "input file not specified: -i <filename>, use -h for help" 2
     fi
 
     if [[ -z ${output_filepath} ]]; then
-        exit_with_error "missing command line argument: -o output.mzML, use -h for help" 2
+        exit_with_error "output file not specified: -o <filename>, use -h for help" 2
     fi
+
 }
 
 parse_args $@
@@ -69,4 +97,4 @@ output_dir=$(absolute_filepath $( dirname "${output_filepath}" ))
 input_filename=$( basename "${input_filepath}" )
 output_filename=$( basename "${output_filepath}" )
 
-exec docker run --rm -v "${input_dir}:/input/" -v "${output_dir}:/output/" bjpop/hitime score -i "/input/${input_filename}" -o "/output/${output_filename}"
+exec docker run --rm -v "${input_dir}:/input/" -v "${output_dir}:/output/" bjpop/hitime ${mode} -i "/input/${input_filename}" -o "/output/${output_filename}" ${remaining_args}
